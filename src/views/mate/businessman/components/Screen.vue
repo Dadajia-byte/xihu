53f1db64af47700c89b15833f864c18c19588d26
RayWang <1143983595@qq.com>
2024-03-05 23:20:48 +0800

    fix: 解决冲突

--- src/views/mate/businessman/components/Screen.vue
+++ src/views/mate/businessman/components/Screen.vue
@@ -1,15 +1,5 @@
 <template>
   <div class="screen-box" ref="vantaRef">
-    <<<<<<< HEAD
-    <h1 class="title_1">
-      <span class="wow fadeInLeft">虚拟数字展馆</span>
-      <span class="wow fadeInDown">&</span>
-      <span class="wow fadeInRight">AR展厅</span>
-    </h1>
-    <h1 class="title_2 wow fadeInUp">
-      <span v-for="item in curStrs">{{ item }}</span>
-    </h1>
-    =======
     <div class="title_1">
       <div class="wow fadeInLeft">展商合作</div>
     </div>
@@ -17,22 +7,14 @@
     <div class="title_2 wow fadeInUp">
       <span v-for="item in curStrs">{{ item }}</span>
     </div>
-    >>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
   </div>
 </template>
 
 <script setup lang="ts">
-<<<<<<< HEAD
-// @ts-ignore
-import clouds from 'vanta/dist/vanta.clouds.min'
-import { onMounted, onUnmounted, ref } from 'vue'
-// @ts-ignore
-=======
 import * as three from 'three'
 //@ts-ignore
 import rings from 'vanta/dist/vanta.rings.min'
 import { onMounted, onUnmounted, ref } from 'vue'
->>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
 import WOW from 'wow.js'
 import videjs from 'video.js'
 
@@ -52,37 +34,22 @@ const initWOW = () => {
 const vantaEffect = ref()
 const vantaRef = ref()
 const { curStrs, startCoding, destroyCoding } =
-<<<<<<< HEAD
-  useCoding('展商风采——带您云游数字展馆')
-
-const initVanta = () => {
-  vantaEffect.value = clouds({
-    el: vantaRef.value,
-=======
   useCoding('展商风采——助力数字安全')
 
 const initVanta = () => {
   vantaEffect.value = rings({
     el: vantaRef.value,
     THREE: three,
->>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
     minHeight: 200.0,
     minWidth: 200.0,
-<<<<<<< HEAD
-    skyColor: 0x4ba8d9,
-    cloudColor: 0xaabfe3,
-    sunGlareColor: 0x2a2a26,
-    speed: 1.4,
-=======
     scale: 1.0,
     scaleMobile: 1.0,
     backgroundAlpha: 1,
     backgroundColor: 0x202428,
     color: 0x88ff00,
->>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
   })
 }
 
@@ -90,8 +57,6 @@ onMounted(() => {
   initVanta()
   initWOW()
   startCoding()
-<<<<<<< HEAD
-=======
   myPlayer.value = videjs(
     videoPlayer.value,
     {
@@ -115,19 +80,15 @@ onMounted(() => {
       myPlayer.value.log('play.....')
     },
   )
->>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
 })
 
 onUnmounted(() => {
   if (vantaEffect.value) {
     vantaEffect.value.destroy()
   }
-<<<<<<< HEAD
-=======
   if (myPlayer.value) {
     myPlayer.value.dispose()
   }
->>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
 })
 </script>
 
@@ -140,19 +101,6 @@ onUnmounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
-<<<<<<< HEAD
-  padding: 15% 20%;
-  gap: 0.6667rem;
-
-  .title_1 {
-    font-size: 1.1rem;
-    display: flex;
-    gap: 0.3333rem;
-  }
-
-  .title_2 {
-    font-size: 0.5rem;
-=======
   padding: 3% 20%;
   .title_1 {
     font-size: 1.1rem;
@@ -167,7 +115,6 @@ onUnmounted(() => {
     height: 7.5rem;
     margin-top: 0.25rem;
     margin-bottom: 0.625rem;
->>>>>>> 296e24bd4209c01c2d7296a25be3ba6252f8b87c
   }
 }
 </style>
